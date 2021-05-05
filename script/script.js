a77dc6f923fdf1a51feeb709879b335c957ec728
n-desantis92 <dsnwebdev@gmail.com>
2021-05-05 17:24:02 +0200

    aggiunta chiamata axios al api del database

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ var app = new Vue ({
   mounted: function() {
 
     console.log(this.genere);
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    axios.get('http://localhost/php-ajax-dischi/api.php')
     .then( (risposta) => {
 
       const albumsOrdered = risposta.data.response.sort(function(a,b){return a.year - b.year});
